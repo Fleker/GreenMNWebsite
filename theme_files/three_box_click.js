8c60d14869f5c1fecf54d1192cab8aa3870e4a84
Ian Johnson <person.uwsome@gmail.com>
2015-06-13 00:12:01 -0500

    Adding theme files for site
    Signed-off-by: Ian Johnson <person.uwsome@gmail.com>

---
+++ theme_files/three_box_click.js
@@ -0,0 +1,15 @@
+$("div.grey").click(
+	function()
+	{
+		window.open("www.google.com");
+	}
+);
+
+
+
+$("div.donateclickpage").click(
+  	function()
+  {
+    window.open("www.wolfram.com");
+  }
+  );
